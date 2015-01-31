eeb598b374c668a94f90fabbf52d5d306cda2882
PetrKryslUCSD <pkrysl@ucsd.edu>
2015-01-31 13:20:01 -0800

    Additional (thermal/mechanical) examples

--- test/test_linear_deformation.jl
+++ test/test_linear_deformation.jl
@@ -411,7 +411,7 @@ function  Twisted_beam(dir)
     #println("displacement =$(theutip[dir]) as compared to converged $uzex")
     @test    abs(theutip[dir]-uzex)<1.0e-5
     
-#println("$( abs(theutip[dir]-uzex)  )")
+    #println("$( abs(theutip[dir]-uzex)  )")
     # # Write out mesh with displacements
     # dadd!(modeldata,postprocessing=dmake(file=  "twisted_beam"))
     # modeldata=JFinEALE.DeformationLinearAlgorithmModule.exportdeformation(modeldata)
@@ -428,6 +428,126 @@ function  Twisted_beam(dir)
 
 end
 Twisted_beam(2)
- Twisted_beam(3)
-   
+Twisted_beam(3)
+
 end
+
+module m # pub_thick_pipe_axi()
+
+using JFinEALE
+using Base.Test
+
+# NAFEMS LE11 benchmark with Q4 elements.
+# # This is a test recommended by the National Agency for Finite Element
+# # Methods and Standards (U.K.): Test LE11 from NAFEMS Publication TNSB,
+# # Rev. 3, “The Standard NAFEMS Benchmarks,” October 1990.  
+# #
+# # Target solution: Direct stress,  = –105 MPa at point A.
+#function  LE11NAFEMS()
+# Parameters:
+Ea= 210000*phun("MEGA*Pa")
+nua= 0.3;
+alphaa=2.3e-4;              # thermal expansion coefficient
+sigmaA=-105*phun("MEGA*Pa")
+nref= 1;                        # how many times should we refine the mesh?
+X=[1.     0.;#A
+   1.4    0.;#B
+   0.995184726672197   0.098017140329561;
+   1.393258617341076 0.137223996461385;
+   0.980785  0.195090;#
+   1.37309939 0.27312645;
+   0.956940335732209   0.290284677254462
+   1.339716470025092 0.406398548156247
+   0.9238795  0.38268;#C
+   1.2124  0.7;#D
+   0.7071  0.7071;#E
+   1.1062  1.045;#F
+   0.7071  (0.7071+1.79)/2;#(E+H)/2
+   1.      1.39;#G
+   0.7071  1.79;#H
+   1.      1.79;#I
+   ]*phun("M")
+   tolerance =1.e-6*phun("M")
+   ##
+   # Note that the material object needs to be created with the proper
+   # model-dimension reduction in mind.  In this case that is the axial symmetry
+   # assumption.
+   mr=DeformationModelReduction2DAxisymm
+
+
+
+fens=FENodeSet(X);
+fes=FESetQ4(conn=[1 2 4 3; 3 4 6 5; 5 6 8 7; 7 8 10 9; 9 10 12 11; 11 12 14 13; 13 14 16 15]);
+for ref=1:nref
+    fens,fes=Q4refine(fens,fes);
+    list=fenodeselect(fens,distance=1.0+0.1/2^nref, from=[0. 0.], inflate=tolerance);
+    fens.xyz[list,:]= JFinEALE.MeshUtilModule.ontosphere(fens.xyz[list,:],1.0);
+end 
+fes.axisymm=(mr== DeformationModelReduction2DAxisymm)  # note that this reflects the chosen model reduction
+
+#     File =  "mesh.vtk"
+# vtkexportmesh(File, fens, fes)
+
+# now we create the geometry and displacement fields
+geom = NodalField(name ="geom",data =fens.xyz)
+u = NodalField(name ="u",data =zeros(size(fens.xyz,1),2)) # displacement field
+
+# Apply EBC's
+l1=fenodeselect(fens,box=[-Inf Inf 0 0],inflate=tolerance)
+setebc!(u,l1,trues(length(l1)),l1*0+2,[0.0])    
+l1=fenodeselect(fens,box=[-Inf Inf 1.79  1.79],inflate=tolerance)
+setebc!(u,l1,trues(length(l1)),l1*0+2,[0.0])    
+applyebc!(u)
+numberdofs!(u)
+
+# Temperature field
+dT =NodalField(name="dT",data=reshape(fens.xyz[:,1]+fens.xyz[:,2],size(fens.xyz,1),1));
+
+
+# Property and material
+material=MaterialDeformationLinear (PropertyDeformationLinearIso(E=Ea,nu=nua,CTE=alphaa))
+
+femm = FEMMDeformationLinear(FEMMBase(fes, GaussRule(order=2,dim=2)), material)
+
+K =stiffness(mr, femm, geom, u)
+F = thermalstrainloads(mr, femm, geom, u, dT)
+#K=cholfact(K)
+U=  K\F
+scattersysvec!(u,U[:])
+
+nA =fenodeselect(fens,box=JFFlt[1.0  1.0 0.0 0.0],inflate=tolerance);
+
+fld= fieldfromintegpoints(mr, femm, geom, u, dT, :Cauchy, 2)
+
+
+# File =  "LE11NAFEMS_Q4_sigmay.vtk"
+# vtkexportmesh(File, fens, fes; scalars=fld.values,scalars_name ="sigmay", vectors=u.values,vectors_name="u")
+
+sA = fld.values[nA]/phun("MEGA*Pa")
+sAn = fld.values[nA]/sigmaA
+#println("Stress at point A: $(sA) i. e.  $( sAn*100  )% of reference value")
+@test abs(sA[1]- (-86.1980522942295))<1.e-5
+
+pt=1
+function inspector(idat,out,loc,pc)
+    global pt
+    if (pt==1)
+        @test norm(loc-[1.0419351909219474 0.010804307080807748])<1.e-7
+        @test norm(reshape(out,1,4)-[2.293918786792204e6 -8.730762977944845e7 5.367202312538694e7 -1.1411272707766877e6])<1.e-2
+        pt=pt+1
+    end
+    return idat
+end
+
+fen2fe =FENodeToFEMap(fes.conn,nfens(geom))
+inspectintegpoints(mr,femm, geom, u, dT,  fen2fe.map[nA[1]],
+                   inspector, []; output=:Cauchy)
+
+#finealemesh(fens,fes,"meshmfile")
+
+# end
+# LE11NAFEMS()
+
+end
+
+
